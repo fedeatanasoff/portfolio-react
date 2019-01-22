0514a2ff51ac0ce30d294ea87f80de3b98df430b
fedeatanasoff <fede.atanasoff@gmail.com>
2019-01-22 16:22:56 -0300

    contact form funcionado con axios y formcarry. simulacion funcionando

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,25 +10,39 @@ class Contact extends Component {
       email: "",
       asunto: "",
       mensaje: "",
-      respuestaForm: ""
+      respuestaForm: "",
+      loading: false
     };
   }
 
   handleForm = e => {
-    axios
-      .post("https://formcarry.com/s/97jxwL-7kNns", this.state, {
-        headers: { Accept: "application/json" }
-      })
-      .then(response => {
-        console.log(response);
-        this.setState({ respuestaForm: response });
-      })
-      .catch(error => {
-        console.log("error =>", error);
-        this.setState({ respuestaForm: error });
-      });
+    // this.setState({ respuestaForm: "", loading: true });
+    // axios
+    //   .post("https://formcarry.com/s/97jxwL-7kNn", this.state, {
+    //     headers: {
+    //       Accept: "application/json"
+    //     }
+    //   })
+    //   .then(response => {
+    //     console.log(response);
+    //     this.setState({ respuestaForm: response, loading: false });
+    //   })
+    //   .catch(error => {
+    //     console.log("error =>", error);
+    //     this.setState({ respuestaForm: error, loading: false });
+    //   });
 
     e.preventDefault();
+    let condicion = false;
+
+    this.setState({ respuestaForm: "", loading: true });
+    setTimeout(() => {
+      if (condicion) {
+        this.setState({ respuestaForm: { status: 200 }, loading: false });
+      } else {
+        this.setState({ respuestaForm: { status: 200 }, loading: false });
+      }
+    }, 2000);
   };
 
   handleFields = e => {
@@ -39,15 +53,10 @@ class Contact extends Component {
 
   render() {
     console.log("respuesta form =>", this.state.respuestaForm);
+
     return (
       <section id="contact">
         <div className="row section-head">
-          {/* <div className="two columns header-col">
-            <h1>
-              <span>nada</span>
-            </h1>
-          </div> */}
-
           <div className="twelve columns">
             <h1 className="h1-titulo">contacto</h1>
             <p className="">
@@ -59,7 +68,6 @@ class Contact extends Component {
             </p>
           </div>
         </div>
-
         <div className="row">
           <div className="twelve columns">
             {this.state.respuestaForm.status === 200 ? (
@@ -71,6 +79,7 @@ class Contact extends Component {
             ) : (
               <form onSubmit={this.handleForm}>
                 <fieldset>
+                  {/* --- nombre input */}
                   <div>
                     <label htmlFor="nombre">
                       Nombre <span className="required">*</span>
@@ -81,51 +90,70 @@ class Contact extends Component {
                       id="contactName"
                       name="nombre"
                       onChange={this.handleFields}
+                      placeholder="ingrese su nombre"
+                      required
                     />
                   </div>
-
+                  {/* --- email input */}
                   <div>
                     <label htmlFor="email">
                       Email <span className="required">*</span>
                     </label>
                     <input
-                      type="text"
+                      type="email"
                       size="35"
                       id="contactEmail"
                       name="email"
+                      placeholder="ingrese su email"
                       onChange={this.handleFields}
+                      required
                     />
                   </div>
-
+                  {/* --- asunto input */}
                   <div>
-                    <label htmlFor="asunto">Asunto</label>
+                    <label htmlFor="asunto">
+                      Asunto <span className="required">*</span>
+                    </label>
                     <input
                       type="text"
                       size="35"
                       id="contactSubject"
                       name="asunto"
                       onChange={this.handleFields}
+                      placeholder="ingrese un asunto"
+                      required
                     />
                   </div>
-
+                  {/* --- mensaje input */}
                   <div>
                     <label htmlFor="mensaje">
                       Mensaje <span className="required">*</span>
                     </label>
                     <textarea
                       cols="50"
-                      rows="15"
+                      rows="1"
                       id="contactMessage"
                       name="mensaje"
                       onChange={this.handleFields}
+                      placeholder="ingrese aqui su consulta "
+                      required
                     />
                   </div>
-
+                  {/* --- Button */}
                   <div>
-                    <button className="submit">Enviar</button>
-                    <span id="image-loader">
-                      <img alt="" src="images/loader.gif" />
-                    </span>
+                    <button
+                      className="submit"
+                      disabled
+                      disabled={this.state.loading}
+                    >
+                      {this.state.loading ? (
+                        <span className="spinner-button">
+                          <i className="fas fa-spinner fa-spin" />
+                        </span>
+                      ) : (
+                        "Enviar"
+                      )}
+                    </button>
                   </div>
                 </fieldset>
               </form>
